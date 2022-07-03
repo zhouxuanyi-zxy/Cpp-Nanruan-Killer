0abfe3d9ecad8c4d00569fa99796fc4b22be3978
zhouxuanyi-zxy <69704410+zhouxuanyi-zxy@users.noreply.github.com>
2022-07-03 18:41:31 +0800

    fix some bug

--- Cpp_Nanruan_Killer.cpp
+++ Cpp_Nanruan_Killer.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main(){
     cout << "使用pssuspend挂起进程" << endl;
-    system("./pssuspend.exe StudentMain.exe");
+    system("pssuspend.exe StudentMain.exe");
     cout << "使用taskkill结束进程" << endl;
-    system("./taskkill /F /T /IM StudentMain.exe");
+    system("taskkill /F /T /IM StudentMain.exe");
     cout << "使用pskill结束进程" << endl;
-    system("./pskill.exe -t StudentMain.exe");
+    system("pskill.exe -t StudentMain.exe");
     cout << "使用ntsd结束进程" << endl;
-    system("./ntsd.exe -c q -pn StudentMain.exe");
+    system("ntsd.exe -c q -pn StudentMain.exe");
     return 0;
 }
