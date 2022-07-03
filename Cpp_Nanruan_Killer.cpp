fe75afdce113d534289bbf103b40f753b3e2af4d
zhouxuanyi-zxy <69704410+zhouxuanyi-zxy@users.noreply.github.com>
2022-07-03 17:15:41 +0800

    Cpp-Nanruan-Killer v1.00

---
+++ Cpp_Nanruan_Killer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// #include <bits/stdc++.h>
+// #include <Windows.h>
+using namespace std;
+
+int main(){
+    cout << "使用pssuspend挂起进程" << endl;
+    system("./pssuspend.exe StudentMain.exe");
+    cout << "使用taskkill结束进程" << endl;
+    system("./taskkill /F /T /IM StudentMain.exe");
+    cout << "使用pskill结束进程" << endl;
+    system("./pskill.exe -t StudentMain.exe");
+    cout << "使用ntsd结束进程" << endl;
+    system("./ntsd.exe -c q -pn StudentMain.exe");
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Cpp-Nanruan-Killer
+
 基于c++的Python-Nanruan-Killer实现
+
+代码十分简单
+
+使用时将ntsd.exe、pskill.exe和pssuspend.exe放在和主程序一个目录下
+
+详见此项目的Python版[Python-Nanruan-Killer](https://github.com/zhouxuanyi-zxy/Python-Nanruan-Killer)的介绍
+
+目前已实现除了最小化窗口的其它功能
+
+###### 烫知识:其实cpp版本的Nanruan-Killer才是最初的版本
+# 版本更新历史
+
+v1.00 (2022.7.3)
+
+1、实现了除了最小化窗口的其它功能
